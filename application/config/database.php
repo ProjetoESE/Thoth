40a2c56996ffadb3f4591ad9916f84d69763a865
Guilherme Bolfe <guilhermebolfe11@gmail.com>
2019-03-28 19:39:53 -0300

    Config Server

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'mysql.hostinger.com.br',
-	'username' => 'u648750589_thoth',
-	'password' => 'L3ss3Unipamp@',
-	'database' => 'u648750589_thoth',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'thoth',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
