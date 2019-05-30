5d409be55ae56d86a0773d8e687d1e96b8fde868
Guilherme Bolfe <guilhermebolfe11@gmail.com>
2019-05-30 16:29:21 -0300

    Remove

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,8 +3,6 @@
 <footer class="footer">
 	<div class="container-fluid">
 		<div class="form-inline justify-content-between">
-			<small class="text-center text-muted">&copy; 2018 by <a target="_blank"
-																	href="http://lesse.com.br">Lesse</a></small>
 			<div>
 				<a target="_blank" href="https://github.com/ProjetoESE/Thoth"><i class="fab fa-github"></i></a>
 				<a target="_blank" href="https://github.com/ProjetoESE/Thoth/issues"><i class="fas fa-bug"></i></a>
